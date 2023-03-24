beef327c0659da257de201b8240ce4bd2c6e781f
jubomi <bomi050623@gmail.com>
2023-03-25 01:09:04 +0900

    📝 :: 마이페이지, 글 작성 페이지 추가

---
+++ Journal/components/MyPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {SafeAreaView, View, Text} from 'react-native';
+
+export default function MyPage() {
+  return (
+    <SafeAreaView>
+      <Text>This is MyPage</Text>
+    </SafeAreaView>
+  );
+}
---
+++ Journal/components/PostPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {SafeAreaView, View, Text} from 'react-native';
+
+export default function PostPage() {
+  return (
+    <SafeAreaView>
+      <Text>This is PostPage</Text>
+    </SafeAreaView>
+  );
+}
